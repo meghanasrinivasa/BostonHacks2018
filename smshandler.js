d1a7f137a71efc8fae9aca08cb26dbb223e6314a
DESKTOP-J41FMKN\Mark <mark.drobnjak@gmail.com>
2018-11-10 18:17:54 -0500

    PUSHHHH

--- smshandler.js
+++ smshandler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.smsreceived = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
