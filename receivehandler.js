82970b0c577b8783be819bc3fd08929e43b2ec5b
DESKTOP-J41FMKN\Mark <mark.drobnjak@gmail.com>
2018-11-11 00:39:55 -0500

    haha

--- receivehandler.js
+++ receivehandler.js
@@ -1,10 +1,12 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
+module.exports.sms = async (event, context) => {
+
+    console.log("body : " + JSON.parse(event.body.Body))
+    return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: '',
       input: event,
     }),
   };
@@ -12,5 +14,3 @@ module.exports.hello = async (event, context) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
-
-
